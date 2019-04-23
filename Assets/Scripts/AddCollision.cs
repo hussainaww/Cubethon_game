b66ff5af9a2d709f890b19d05c7da82c80412ab2
hussainaww <31806160+hussainaww@users.noreply.github.com>
2019-04-23 14:58:38 -0700

    Add files via upload

---
+++ Assets/Scripts/AddCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class AddCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collisionInfo) {
+        if ( collisionInfo.collider.tag == "Obstacle" ) {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+            
+        }
+    }
+}
---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    // Start is called before the first frame update
+   void OnTriggerEnter () 
+   {
+       gameManager.CompleteLevel();
+   }
+}
